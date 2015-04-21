aaebb0cb1d6756316af7a9d9a63b88a5dba34aa4
Shane Gibbs <shane@hands.net.nz>
2015-04-20 21:26:25 -0500

    Extract mock responses to file

--- src/main/java/com/gibbsdevops/alfred/utils/rest/MockJsonRestClient.java
+++ src/main/java/com/gibbsdevops/alfred/utils/rest/MockJsonRestClient.java
@@ -1,7 +1,9 @@
 package com.gibbsdevops.alfred.utils.rest;
 
 import com.google.common.collect.Maps;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class MockJsonRestClient implements JsonRestClient {
@@ -25,8 +27,37 @@ public class MockJsonRestClient implements JsonRestClient {
         map.put(request, new RestResponse(response));
     }
 
+    public void addRequestFromJar(RestRequest request, Class c, String resource) {
+        try {
+            map.put(request, new RestResponse(
+                    IOUtils.toString(c.getResourceAsStream(resource))
+            ));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load resource: " + resource, e);
+        }
+    }
+
     public void reset() {
         map.clear();
     }
 
+    public static class Builder {
+
+        private MockJsonRestClient mockJsonRestClient;
+        private Class clazz;
+
+        public Builder(Class clazz) {
+            this.mockJsonRestClient = new MockJsonRestClient();
+            this.clazz = clazz;
+        }
+
+        public void get(String url, String resource) {
+            mockJsonRestClient.addRequestFromJar(RestRequest.get(url), clazz, resource);
+        }
+
+        public MockJsonRestClient getMockJsonRestClient() {
+            return mockJsonRestClient;
+        }
+
+    }
 }
