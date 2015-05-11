986f74a1e7671ea86eb5dd3a53b1c0cca2486471
Shane Gibbs <shane@hands.net.nz>
2015-05-11 16:12:48 -0500

    Fix js tests

--- src/test/resources/webapp/js/alfred/commitTest.js
+++ src/test/resources/webapp/js/alfred/commitTest.js
@@ -15,22 +15,22 @@ QUnit.test( "find with data", function( assert ) {
   var commit = Alfred.Commit.find(123, { 'id': 123, 'sender': 2, 'repo': 3 });
   assert.ok(commit != null);
   assert.strictEqual( commit, Alfred.CommitsById[123], "CommitsById" );
-  assert.equal( commit.get('sender_id'), 2, "sender_id" );
-  assert.equal( commit.get('repo_id'), 3, "repo_id" );
+  assert.equal( commit.get('sender'), 2, "sender" );
+  assert.equal( commit.get('repo'), 3, "repo" );
 });
 
 QUnit.test("find with id", function(assert) {
   var commit = Alfred.Commit.find(123);
   assert.ok(commit != null);
   assert.strictEqual( commit, Alfred.CommitsById[123], "CommitsById" );
-  assert.equal( commit.get('sender_id'), 2, "sender_id" );
-  assert.equal( commit.get('repo_id'), 3, "repo_id" );
+  assert.equal( commit.get('sender'), 2, "sender" );
+  assert.equal( commit.get('repo'), 3, "repo" );
 });
 
 QUnit.test( "find with updated data", function( assert ) {
   var commit = Alfred.Commit.find(123);
   var commit_new = Alfred.Commit.find(123, { 'id': 123, 'sender': 2, 'repo': 3 });
   assert.strictEqual(commit, commit_new, "returns same object" );
-  assert.equal( commit.get('sender_id'), 2, "sender_id" );
-  assert.equal( commit.get('repo_id'), 3, "repo_id" );
+  assert.equal( commit.get('sender'), 2, "sender" );
+  assert.equal( commit.get('repo'), 3, "repo" );
 });
