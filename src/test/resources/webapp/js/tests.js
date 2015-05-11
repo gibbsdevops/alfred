d00a70ba867f577aab0badcb4c5cd9607e5715b0
Shane Gibbs <shane@hands.net.nz>
2015-05-11 16:35:18 -0500

    Fix js tests

--- src/test/resources/webapp/js/tests.js
+++ src/test/resources/webapp/js/tests.js
@@ -8,9 +8,9 @@ Alfred.debug = function(msg) {
 
 Alfred.Fixtures = {};
 Alfred.Fixtures.Finder = {};
-Alfred.Fixtures.Finder['/api/job/122'] = { 'id': 122, 'commit': 123 };
-Alfred.Fixtures.Finder['/api/commit/123'] = { 'id': 123, 'sender': 2,  'repo': 3, 'committer': 4, 'author': 4, 'pusher': 4, 'hash': 'abc' };
-Alfred.Fixtures.Finder['/api/repo/3'] = { 'id': 3, 'name': 'myrepo', 'owner': 2 };
+Alfred.Fixtures.Finder['/api/job/122'] = { 'id': 122, 'commit_id': 123 };
+Alfred.Fixtures.Finder['/api/commit/123'] = { 'id': 123, 'sender_id': 2,  'repo_id': 3, 'committer_id': 4, 'author_id': 4, 'pusher_id': 4, 'hash': 'abc' };
+Alfred.Fixtures.Finder['/api/repo/3'] = { 'id': 3, 'name': 'myrepo', 'owner_id': 2 };
 Alfred.Fixtures.Finder['/api/user/2'] = { 'id': 2, 'name': 'UserName' };
 Alfred.Fixtures.Finder['/api/person/4'] = { 'id': 4, 'name': 'PersonName', 'email': 'PersonEmail' };
 
